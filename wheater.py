b1fb29bf01bd3273c00a16d0cfa0ef210df80716
bzzzbzzzbzz <65319137+bzzzbzzzbzz@users.noreply.github.com>
2023-08-03 19:16:08 +0200

    Update wheater.py

--- wheater.py
+++ wheater.py
@@ -1,9 +1,15 @@
 import requests
+from urllib.parse import urljoin
 
-cities = ['London', 'Аэропорт_Шереметьево', 'череповец?lang=ru&?M&?n&?q&?T']
+website_url = 'https://wttr.in/'
+cities = {
+    'London': { },
+    'Аэропорт_Шереметьево': { },
+    'череповец': {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': ''}
+}
 
-for city in cities:
-    url = 'https://wttr.in/{}'
-    res = requests.get(url.format(city))
+for city, params in cities.items():
+    url = urljoin(website_url, city)
+    res = requests.get(url, params=params)
     res.raise_for_status()
     print(res.text)
